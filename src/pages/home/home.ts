6aa6ce184969a51d9ffcc7bbf8f3bbd67ba4d64b
robsonlima <robsonklima@gmail.com>
2021-04-17 00:57:05 -0300

    REF

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Config } from '../../models/config';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {}
+  acoes = [];
+  
+  constructor(public navCtrl: NavController) {
+    this.acoes = Config.ACOES;
+  }
 }
